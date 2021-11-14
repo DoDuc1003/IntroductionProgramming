a8c1fdbb78d0ed017a123944827c00365a2692a6
Merlin Do <doduc9569@gmail.com>
2021-11-14 21:31:15 +0700

    draw triangle

---
+++ Introduction/DrawTriangle.cpp
@@ -0,0 +1,14 @@
+//
+// Created by ducdo on 11/14/2021.
+//
+
+#include <iostream>
+using namespace std;
+int main ()
+{
+    cout <<"  *  \n";
+    cout <<" *** \n";
+    cout <<"*****\n";
+    return 0;
+
+}
